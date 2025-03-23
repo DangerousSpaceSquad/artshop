ceb7cc8e379c75fc8b04e7a12572fb113f121f1b
Kyle Reto <kylereto@gmail.com>
2025-03-22 20:22:01 -0400

    Added quick build option

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 TAG=artshop:latest
-./deployment/scripts/containerize.sh $TAG && \
-./deployment/scripts/run_docker.sh $TAG
+MODE=$1
+if [ -z "$MODE" ]
+then
+    echo "To set the build environment, run this script with \`DEBUG\` (faster, debugging tools) or \`STAGE\` (slower, realistic enviroment) as the first argument. Defaulting to \`STAGE\`."
+    MODE=STAGE
+fi
+if [ "$MODE" == "STAGE" ]
+then
+    ./deployment/scripts/containerize.sh $TAG && \
+    ./deployment/scripts/run_docker.sh $TAG
+elif [ "$MODE" == "DEBUG" ]
+then
+    ./deployment/scripts/run_no_docker.sh
+else
+    echo "Unrecognized parameter $MODE, exiting..."
+    exit 1
+fi
