4b3aa55498260e5fbeb82ae4e5006834fe7bd254
ChristopherADean <ceecchrisdean@gmail.com>
2025-03-26 12:19:47 -0400

    Carousel beginnings

--- artshop.client/src/App.jsx
+++ artshop.client/src/App.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { ImageSlider } from "./ImageSlider";
 import Navbar from "./Navbar";
-import carousel1 from "./assets/JaneDoeFinalNoText.jpg"
-import carousel2 from "./assets/Day2RosalinaShaded.jpg"
-import carousel3 from "./assets/Day3FeliciaSticker.jpg"
-import carousel4 from "./assets/Day10MirkoFinal.jpg"
-import carousel5 from "./assets/Day11MaiShiranuiInked.jpg"
+import carousel1 from "./assets/Demo1.jpg"
+import carousel2 from "./assets/Demo2.jpg"
+import carousel3 from "./assets/Demo3.jpg"
+import carousel4 from "./assets/Demo4.jpg"
+import carousel5 from "./assets/Demo5.jpg"
 
 const CAROUSEL_IMAGES = [carousel1, carousel2, carousel3, carousel4, carousel5]
 
