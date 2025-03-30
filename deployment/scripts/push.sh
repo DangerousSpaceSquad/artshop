034e06c8346a826ac2d183b324ec888780d9dca2
Kyle Reto <kylereto@gmail.com>
2025-03-30 19:21:02 -0400

    Added ECR GH Action for master (#15)

--- deployment/scripts/push.sh
+++ deployment/scripts/push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Push the docker image to external repositories (i.e. ECR)
+# $1 is the tag to use for the image
 if [ -z "${AWS_ID}" ]
 then
     echo "Please set the AWS_ID environment variable to the 12-digit AWS account ID, i.e. run 'export AWS_ID={12-digit ID}.'"
     exit 1
 fi
-TAG=artshop:prod
+TAG=$1
 ECR_TAG=${AWS_ID}.dkr.ecr.us-east-1.amazonaws.com/$TAG
+echo $TAG
 
 echo "Pushing the container to ECR..."
 aws ecr get-login-password \
@@ -17,4 +19,3 @@ aws ecr get-login-password \
 docker tag $TAG $ECR_TAG
 docker push $ECR_TAG
 echo "Successfully pushed the container to ECR."
-exit 0
