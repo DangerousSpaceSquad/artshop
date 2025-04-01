11746039de8cdda213695f3055de1c68172cd5d4
ChristopherADean <ceecchrisdean@gmail.com>
2025-04-01 15:38:15 -0400

    Added item page and basic shop page. Known bug with item page: choosing size doesn't change style on first click.

--- artshop.client/src/pages/Shop.jsx
+++ artshop.client/src/pages/Shop.jsx
@@ -1,3 +1,21 @@
+import React from 'react';
+import ShopItem from "../ShopItem";
+import './Shop.css'
+
 export default function Home() {
-    return <h1>Shop</h1>
+    return (
+        <div className="shop-grid">
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+            <ShopItem />
+        </div>
+    )
 }
