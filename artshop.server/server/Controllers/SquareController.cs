586a91297494f97dd911d6b484a00f72eb2ccb06
Kyle Reto <kylereto@gmail.com>
2026-02-20 17:15:40 -0500

    Square catalog (#36)
    * Fixed docker setup ignoring environment variables
    
    * Added route to display all items in catalog
    
    * Added quick and dirty routes for catalog item display

--- artshop.server/server/Controllers/SquareController.cs
+++ artshop.server/server/Controllers/SquareController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
 using Square;
 using Square.Checkout.PaymentLinks;
 using Square.Catalog;
+using Square.Core;
+using Square.Catalog.Object;
+
+using artshop.Server.Models;
 
 namespace artshop.Server.Controllers;
 
@@ -11,7 +14,7 @@ namespace artshop.Server.Controllers;
 public class SquareController : Controller
 {
     // A WIP controller for the connection to the Square API. Expect routes to be deleted and added in the near future.
-    private SquareClient client;
+    private readonly SquareClient client;
     public SquareController()
     {
         client = new SquareClient(
@@ -20,22 +23,89 @@ public class SquareController : Controller
             }
         );
     }
-    
-    [HttpGet("ListCatalog")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<CatalogObject>> ListCatalogItems()
-    {
-        var catalogItems = await client.Catalog.ListAsync(
-            new ListCatalogRequest()
-        );
 
-        List<CatalogObject> itemIds = [];
-        await foreach (var item in catalogItems)
+    /// <summary>
+    /// Given a list of catalog object displays, and a dictionary containing their associated images,
+    /// add the image URLs to the catalog objects.
+    /// </summary>
+    /// <param name="catalogObjectDisplays">The list of CatalogObjectDisplay objects to add images to</param>
+    /// <param name="catalogImageDict">The dictionary of CatalogObjectImages to pull image URLs from</param>
+    private static void PairImagesToCatalogDisplayObjects(
+        List<CatalogObjectDisplay> catalogObjectDisplays,
+        Dictionary<string, CatalogObjectImage> catalogImageDict)
+    {
+        foreach (CatalogObjectDisplay displayObject in catalogObjectDisplays)
         {
-            itemIds.Add(item);
+            if (displayObject.ImageId == null || !catalogImageDict.ContainsKey(displayObject.ImageId)) continue;
+            CatalogImage? imageData = catalogImageDict[displayObject.ImageId].ImageData;
+
+            if (imageData == null) continue;
+            if (imageData.Url == null) continue;
+            displayObject.ImageURL = imageData.Url;
+        }
+    }
+
+    /// <summary>
+    /// Retrieve an overview of all items in the catalog for display.
+    /// </summary>
+    /// <returns>A list of CatalogObjectDisplay objects, which have minimal information about each item in the Square catalog</returns>
+    /// <exception cref="SquareException">If an item retrieved from the Square API doesn't match the expected type</exception>
+    [HttpGet("ListCatalogItemsForDisplay")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<List<CatalogObjectDisplay>> ListCatalogItemsForDisplay()
+    {
+        Pager<CatalogObject> catalogItems = await client.Catalog.ListAsync(
+            new ListCatalogRequest{
+                Types = "ITEM,IMAGE"
+            }
+        );
+        List<CatalogObjectDisplay> displayObjects = new();
+        Dictionary<string, CatalogObjectImage> catalogImages = new();
+
+        await foreach (CatalogObject item in catalogItems)
+        {
+            if (item.IsItem)
+            {
+                // If the item has no data, skip it
+                CatalogObjectItem retrievedItem = item.AsItem();
+                if (retrievedItem.ItemData == null) continue;
+
+                CatalogObjectDisplay displayObject = new()
+                {
+                    ItemId = retrievedItem.Id,
+                    Name = retrievedItem.ItemData.Name ?? "ERROR: Item name not found",
+                    DescriptionHTML = retrievedItem.ItemData.DescriptionHtml ?? "ERROR: Item description not found",
+                    ImageId = retrievedItem.ItemData.ImageIds?.First()
+                };
+
+                displayObjects.Add(displayObject);
+            }
+            else if (item.IsImage)
+            {
+                catalogImages[item.AsImage().Id] = item.AsImage();
+            }
+            else
+            {
+                throw new SquareException("An object of type IMAGE or ITEM was expected from the Catalog, but an item of a different type was found.");
+            }
         }
 
-        return itemIds;
+        PairImagesToCatalogDisplayObjects(displayObjects, catalogImages);
+
+        return displayObjects;
+    }
+
+    [HttpGet("GetCatalogItem/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCatalogItem(string? id)
+    {
+        var resp = await client.Catalog.Object.GetAsync(
+            new GetObjectRequest{
+                ObjectId = id ?? ""
+            }
+        );
+        return resp == null ? NotFound() : Ok(resp);
     }
 
     [HttpGet("ListLocations")]
--- deployment/scripts/run_docker.sh
+++ deployment/scripts/run_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Run the program using docker
 # $1 is the name of the image to run
+if [ -z "${SQUARE_TOKEN}" ]
+then
+    echo "Square API key not set, some backend features may not work correctly. To fix this, ensure that the 'SQUARE_TOKEN' environment variable is set."
+fi
 docker run -it --rm -p 8080:8080 -e ASPNETCORE_HTTP_PORTS=8080 -e SQUARE_TOKEN=${SQUARE_TOKEN} $1
