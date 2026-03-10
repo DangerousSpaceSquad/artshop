28194391720370dd4c026fe6d44bcbb40704c350
Kyle Reto <kylereto@gmail.com>
2026-03-10 04:39:52 -0400

    Updated Catalog routes to list all item variations, instead of items. (#44)
    Closes #40

---
+++ artshop.server/server/Models/CatalogVariationDisplay.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace artshop.Server.Models;
+
+/// <summary>
+/// A model representing all of the information needed to display a Square item Variation for the Catalog
+/// </summary>
+public class CatalogVariationDisplay
+{
+    public required string ItemId { get; set; }
+    public required string VariationId { get; set; }
+    public required string ItemName { get; set; }
+    [AllowNull]
+    public required string VariationName { get; set; }
+    public required Square.Money Price { get; set; }
+
+    [AllowNull]
+    public string ImageId { get; set; }
+    [AllowNull]
+    public string ImageURL { get; set; }
+    [AllowNull]
+    public string DescriptionHTML { get; set; }
+}
