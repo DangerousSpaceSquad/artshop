910d10526df01611001cfc05b116c97d38f83d82
Christopher Dean <56937450+ChristopherADean@users.noreply.github.com>
2026-04-10 18:05:02 -0400

    Added assets for the image carousel and made it cycle automatically. (#49)

--- artshop.client/src/ImageSlider.tsx
+++ artshop.client/src/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { ChevronLeft, ChevronRight, CircleDot, Circle } from "lucide-react"
 import './ImageSlider.css'
 
@@ -10,38 +10,46 @@ type ImageSliderProps = {
 export function ImageSlider({ imageUrls }: ImageSliderProps) {
     const [imageIndex, setImageIndex] = useState(0)
 
-    function showNextImage(){
+    function showNextImage() {
         setImageIndex(index => {
-            if (index === imageUrls.length-1) return 0
+            if (index === imageUrls.length - 1) return 0
             return index + 1
         })
     }
 
-    function showPrevImage(){
+    function showPrevImage() {
         setImageIndex(index => {
-            if (index === 0) return imageUrls.length-1
+            if (index === 0) return imageUrls.length - 1
             return index - 1
         })
     }
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            showNextImage();
+        }, 5000); 
+        return () => clearInterval(interval);
+    }, []);
+
+
     return (
-        <div style ={{width: "100%", height: "100%", position: "relative"}}>
-            <div style={{width: "100%", height: "100%", display: "flex", overflow: "hidden"}}>
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <div style={{ width: "100%", height: "100%", display: "flex", overflow: "hidden" }}>
                 {imageUrls.map(url => (
-                    <img 
-                    key={url}
-                    src={url}
-                    className="img-slider-img" 
-                    style={{ translate: `${-100 * imageIndex}%`}} 
+                    <img
+                        key={url}
+                        src={url}
+                        className="img-slider-img"
+                        style={{ translate: `${-100 * imageIndex}%` }}
                     />
                 ))}
             </div>
-            
 
-            <button onClick={showPrevImage} className="img-slider-btn" style={{left: 0}}>
+
+            <button onClick={showPrevImage} className="img-slider-btn" style={{ left: 0 }}>
                 <ChevronLeft />
             </button>
-            <button onClick={showNextImage} className="img-slider-btn" style={{right: 0}}>
+            <button onClick={showNextImage} className="img-slider-btn" style={{ right: 0 }}>
                 <ChevronRight />
             </button>
             <div style={{
@@ -53,8 +61,8 @@ export function ImageSlider({ imageUrls }: ImageSliderProps) {
                 gap: ".25rem"
             }}>
                 {imageUrls.map((_, index) => (
-                    <button key={index} className="img-slider-dot-btn" onClick={() => 
-                    setImageIndex(index)}>{index === imageIndex ? <CircleDot /> : <Circle />}</button>
+                    <button key={index} className="img-slider-dot-btn" onClick={() =>
+                        setImageIndex(index)}>{index === imageIndex ? <CircleDot /> : <Circle />}</button>
                 ))}
             </div>
         </div>
