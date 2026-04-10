aab1a60e6abdcfd2480e2811e4c76031598a5347
ChristopherADean <ceecchrisdean@gmail.com>
2026-04-10 16:08:41 -0400

    Added assets for the image carousel and made it cycle automatically.

--- artshop.client/src/pages/Home.jsx
+++ artshop.client/src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import carousel1 from "../assets/Demo1.jpg"
-import carousel2 from "../assets/Demo2.jpg"
-import carousel3 from "../assets/Demo3.jpg"
-import carousel4 from "../assets/Demo4.jpg"
-import carousel5 from "../assets/Demo5.jpg"
+import carousel1 from "../assets/LaunchCarouselItem.png"
+import carousel2 from "../assets/StickersCarouselItem.png"
+import carousel3 from "../assets/PrintsCarouselItem.png"
+
 
 import { ImageSlider } from "../ImageSlider";
 import ShopItem from "../ShopItem";
 import './Home.css'
 
-const CAROUSEL_IMAGES = [carousel1, carousel2, carousel3, carousel4, carousel5]
+const CAROUSEL_IMAGES = [carousel1, carousel2, carousel3]
 
 export default function Home() {
     return (
