1d4dc85827f2e9ff5f2dfc6e8f48dbc568054e58
ChristopherADean <ceecchrisdean@gmail.com>
2026-04-13 16:29:41 -0400

    Added shopping cart styling and made it function for mobile. Replaced placeholder content on FAQ and Porfolio pages.

--- artshop.client/src/pages/Portfolio.jsx
+++ artshop.client/src/pages/Portfolio.jsx
@@ -1,14 +1,19 @@
 import './Portfolio.css'
 import { ImageGallery } from "../ImageGallery";
 
-import gal1 from "../assets/bigfoot.jpg"
-import gal2 from "../assets/Demo3.jpg"
-import gal3 from "../assets/Demo4.jpg"
-import gal4 from "../assets/Demo1.jpg"
-import gal5 from "../assets/Demo2.jpg"
 
-
-const GALLERY_IMAGES = [gal1, gal2, gal3, gal4, gal5, gal1, gal2, gal3, gal4, gal5]
+const GALLERY_IMAGES = [
+    '../assets/GalleryImages/Bowsette2024Final.jpg',
+    '../assets/GalleryImages/BulmaFinal.jpg',
+    '../assets/GalleryImages/Cammy2026Final.jpg',
+    '../assets/GalleryImages/Chun-li2024FinalV3.jpg',
+    '../assets/GalleryImages/Day2RosalinaShaded.jpg',
+    '../assets/GalleryImages/Day10MirkoFinal.jpg',
+    '../assets/GalleryImages/JaneDoeFinalNoText.jpg',
+    '../assets/GalleryImages/MiguFinal.jpg',
+    '../assets/GalleryImages/SandyBashFinal.jpg',
+    '../assets/GalleryImages/YoruichiFinal.jpg',
+]
 
 export default function Home() {
     return (
