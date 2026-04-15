0dad869a1c58c67712f0a525ce23388e9dec1b8d
KyleReto <kylereto@gmail.com>
2026-04-15 14:39:27 -0400

    Removed dummy test

--- artshop.client/tests/App.test.jsx
+++ artshop.client/tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it } from 'vitest'
-import { render } from '@testing-library/react'
-import App from '../src/App.jsx'
+//import { render } from '@testing-library/react'
+//import App from '../src/App.jsx'
 
 // describe() defines a test suite, which is a collection of tests with a related purpose, like tests for
 // a component, class, feature, function, etc.
@@ -8,7 +8,7 @@ describe('App', () => {
     // it() defines a test case, which is a single test.
     it('renders the App component', () => {
         // render() takes any JSX and renders it. The output of this is sent to "screen"
-        render(<App />);
+        //render(<App />);
         // To see what's rendered, you can use screen.debug(), which prints the output to the console.
         // Alternatively, you can use getByX() to test for values, like below:
         //expect(screen.getByText('Home')).toBeInTheDocument();
