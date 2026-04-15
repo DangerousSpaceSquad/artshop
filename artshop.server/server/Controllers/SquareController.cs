6c83f08dacecc6c11b9f7a2ca570e3dff1bdb287
Kyle Reto <kylereto@gmail.com>
2026-04-14 20:40:20 -0400

    Updated Shop page to show only correct items (#52)
    * Removed duplicates in shop display
    
    * Added filter to exclude in-person-only items from the web store
    
    * List items by lowest price, not highest

--- artshop.server/server/Controllers/SquareController.cs
+++ artshop.server/server/Controllers/SquareController.cs
@@ -126,6 +126,12 @@ public class SquareController : Controller
         {
             if (item.IsItem)
             {
+                // TODO: Currently, it's assumed that items without categories are to be excluded from the web store.
+                    // It would be better to use the ecom_visibility property in Square to choose which items to in/exclude.
+                if ((item.AsItem().ItemData?.Categories?.Count() ?? 0) == 0)
+                {
+                    continue;
+                }
                 var retrievedVariations = GetDisplayVariationsFromItem(item);
                 displayObjects.AddRange(retrievedVariations);
             }
@@ -141,7 +147,19 @@ public class SquareController : Controller
 
         PairImagesToCatalogDisplayObjects(displayObjects, catalogImages);
 
-        return displayObjects;
+        Dictionary<string, CatalogVariationDisplay> filteredVariations = [];
+        
+        foreach (CatalogVariationDisplay item in displayObjects)
+        {
+            if (!filteredVariations.TryGetValue(item.ItemId, out CatalogVariationDisplay? value) || 
+                    item.Price.Amount <= value.Price.Amount)
+            {
+                filteredVariations[item.ItemId] = item;
+            }
+
+        }
+
+        return filteredVariations.Values.ToList();
     }
 
     // TODO: Revise this method to filter the response to just the relevant details
